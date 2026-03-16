df3ba400d65e6c61554f2a784c6568693d8f7bd5
Itumeleng Kefeletswe <37324344+tumikef@users.noreply.github.com>
2026-03-16 19:26:27 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "DevOps CI/CD Pipeline Working!"
+    
+@app.route("/health")
+def health():
+    return {"status": "healthy"}, 200
 
+@app.route("/ready")
+def ready():
+    return {"status": "ready"}, 200
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
