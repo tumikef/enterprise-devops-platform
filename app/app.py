2bbeff8c910fc68fd2d55d8208bd85b798ca6cdf
tumikef <tumikef@gmail.com>
2026-03-19 17:19:54 +0200

    fix the roll issues

--- app/app.py
+++ app/app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "DevOps CI/CD Pipeline Working!"
-    
+
 @app.route("/health")
 def health():
-    return {"status": "healthy"}, 200
+    return jsonify(status="healthy"), 200
 
 @app.route("/ready")
 def ready():
-    return {"status": "ready"}, 200
-    
+    return jsonify(status="ready"), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
