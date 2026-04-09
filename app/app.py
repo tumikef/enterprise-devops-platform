636c34aec507aa386942112a11aedac54d3688c1
tumikef <tumikef@gmail.com>
2026-04-09 19:25:15 +0200

    add helm details

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "DevOps CI/CD Pipeline Working!"
 
+@app.route("/health")
+def health():
+    return {"status": "healthy"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
